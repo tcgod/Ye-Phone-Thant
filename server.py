5457be493466357a29bcb5624e1c4c13a614bb7e
napier4040xxxx <florencejuri.17@gmail.com>
2018-04-04 12:27:58 +0630

    'delete'

---
+++ server.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template
+import json
+
+w = json.load(open("worldl.json"))
+for c in w:
+	c['tld'] = c['tld'][1:]
+page_size = 20
+app = Flask(__name__)
+
+@app.route('/')
+def mainPage():
+	return render_template('index.html',
+		page_number=0,
+		page_size=page_size,
+		w = w[0:page_size])
+
+@app.route('/begin/<b>')
+def beginPage(b):
+	bn = int(b)
+	return render_template('index.html',
+		w = w[bn:bn+page_size],
+		page_number = bn,
+		page_size = page_size
+		)
+
+@app.route('/continent/<a>')
+def continentPage(a):
+	cl = [c for c in w if c['continent']==a]
+	return render_template(
+		'continent.html',
+		length_of_cl = len(cl),
+		cl = cl,
+		a = a
+		)
+
+@app.route('/country/<i>')
+def countryPage(i):
+	return render_template(
+		'country.html',
+		c = w[int(i)])
+
+@app.route('/countryByName/<n>')
+def countryByNamePage(n):
+	c = None
+	for x in w:
+		if x['name'] == n:
+			c = x
+	return render_template(
+		'country.html',
+		c = c)
+
+@app.route('/delete/<n>')
+
+def deleteCountryPage(n):
+	i=0
+	for c in w:
+		if c['name'] == n:
+			break
+
+		i+=1
+
+	del w[i]
+	return render_template('index.html',
+		page_number=0,
+		page_size=page_size,
+		w = w[0:page_size])
+#all deleted country will be back on the list after restarting the server
+
+app.run(host='0.0.0.0', port=5645, debug=True)
+
+
+
+
+
+
