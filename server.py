e21fae89b6bc95aba6a5f837090ad659e4a6e628
tcgod <yephonethant@gmail.com>
2018-04-26 15:00:36 +0630

    Final

--- server.py
+++ server.py
@@ -4,15 +4,11 @@ import json
 
 w = json.load(open("worldl.json"))
 lota = sorted (list(set([c['name'][0] for c in w])))
-
 print (lota)
 for c in w:
 	c['tld'] = c['tld'][1:]
 page_size = 20
-
 l = []
-for i in range(ord('A'), ord('Z')+1):
-	l.append(chr(i))
 app = Flask(__name__)
 
 @app.route('/')
@@ -40,12 +36,8 @@ def continentPage(a):
 		'continent.html',
 		length_of_cl = len(cl),
 		cl = cl,
-<<<<<<< HEAD
 		a = a,
 		lota=lota)
-=======
-		a = a)
->>>>>>> 9e13693b92d14194b270c8c4fdf68b56bcb53baf
 
 @app.route('/country/<i>')
 def countryPage(i):
@@ -76,8 +68,7 @@ def deleteCountryPage(n):
 		page_number=0,
 		page_size=page_size,
 		w = w[0:page_size])
-
-#all deleted country will be back on the list after restarting the server
+	
 @app.route('/createcountry')
 def createCountryByName():
 	return render_template('createcountry.html', c=c)
@@ -108,7 +99,6 @@ def editcountryByNamePage(n):
 	return render_template(
 		'country-edit.html',
 		c = c)
-<<<<<<< HEAD
 
 @app.route('/describewithAlphabetics/<a>')
 def describewithAlphabetics(a):
@@ -119,9 +109,6 @@ def describewithAlphabetics(a):
 		cl = cl,
 		a = a,
 		lota=lota)
-=======
-<<<<<<< HEAD
->>>>>>> 9e13693b92d14194b270c8c4fdf68b56bcb53baf
 
 @app.route('/updatecountrybyname')
 def updatecountryByNamePage():
@@ -139,38 +126,6 @@ def updatecountryByNamePage():
 	return render_template(
 		'country.html',
 		c = c)
-<<<<<<< HEAD
-=======
-
-@app.route('/describewithAlphabetics/<a>')
-def describewithAlphabetics(a):
-	cl = [c for c in w if c['name'][0]==a]
-	return render_template(
-		'continent.html',
-		length_of_cl = len(cl),
-		cl = cl,
-		a = a,
-		lota=lota)
-=======
-
-@app.route('/updatecountrybyname')
-def updatecountryByNamePage():
-	n=request.args.get('name')
-	c = None
-	for x in w:
-		if x['name'] == n:
-			c = x
-	c['capital'] = request.args.get('capital')
-	c['continent'] = request.args.get('continent')
-	c['area'] = int(request.args.get('area'))
-	c['gdp']  = float(request.args.get('gdp'))
-	c['tld']  = request.args.get('tld')
-	c['population']  = int(request.args.get('population'))
-	return render_template(
-		'country.html',
-		c = c)
->>>>>>> dbc18d43cc3dc6a7a4ee7106836d40e2016bf965
->>>>>>> 9e13693b92d14194b270c8c4fdf68b56bcb53baf
 
 app.run(host='0.0.0.0', port=5619, debug=True)
 
